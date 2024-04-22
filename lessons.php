6720ae65cf940338998a139aa0c3dd5689a9f823
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-22 22:25:18 +0300

    adding url normalizing function and leap year function

--- lessons.php
+++ lessons.php
@@ -1,7 +1,27 @@
 <?php
-// Task 1. Trimming function
+// =========== Task 1. Trimming function ==============
 // function truncate($text,$count)
 // {
 //     $res = substr($text,0,$count)."...";
 //     return $res;
+// }
+
+// =========== Task 2. Leap year detection function ==============
+//function isLeapYear($year)
+//{
+//    return $year % 400 == 0 || ($year % 100 != 0 && $year % 4 == 0);
+//}
+
+// =========== Task 3. URL normalizing function ==============
+// function normalizeUrl($url)
+// {
+//     if (strpos($url, 'http://') === 0) {
+//         $domain = substr($url, 7);
+//     } elseif (strpos($url, 'https://') === 0) {
+//         $domain = substr($url, 8);
+//     } else {
+//         $domain = $url;
+//     }
+
+//     return "https://{$domain}";
 // }
