c92f16f670275de5fc58ece014b420e1542b1ec4
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-23 22:31:37 +0300

    Update lessons.php

--- lessons.php
+++ lessons.php
@@ -41,3 +41,19 @@
 //             return null;
 //     }
 // }
+
+// =========== Task 5. Checking arguments for substr function ==============
+// function isArgumentsForSubstrCorrect($str,$index,$length)
+// {
+//     if ($index < 0 || $index >= strlen($str))
+//     {
+//         return false;
+//         }
+//     elseif ($length < 0 || $length + $index > strlen($str))
+//     {
+//         return false;
+//     }
+//     else {
+//         return true;
+//     }
+// }
