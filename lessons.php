ff95362828d863c5502e5fd2991c8539f21cb9bb
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-24 10:43:31 +0300

    adding timestamp function

--- lessons.php
+++ lessons.php
@@ -74,3 +74,10 @@
 //     } 
 //     return $result;
 // }
+
+// =========== Task 7. Timestamp format ==============
+// function getCustomDate($time)
+// {
+//  $fdate = date('d/m/Y', $time);
+//  return $fdate;
+// }
