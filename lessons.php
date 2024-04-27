3ed9dfb041a96d8dfde4949bda2bb974c131c5d9
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-27 16:04:42 +0300

    Update lessons.php

--- lessons.php
+++ lessons.php
@@ -123,3 +123,9 @@
     
 //     return intval($temp);
 // }
+
+// =========== Task 10. Sum of binary digits ==============
+// function binarySum (string $num1, string $num2) {
+//     $sum = bindec($num1) + bindec($num2);
+//     return decbin($sum);
+// }
