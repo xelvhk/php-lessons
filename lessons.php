f3cc5b372b9b54f484f420289f8bb8b410cc3bde
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-29 15:34:24 +0300

    added getting total amount from array

--- lessons.php
+++ lessons.php
@@ -208,3 +208,18 @@
 //     }
 //     return $openBracket == 0;
 // }
+
+// =========== Task 18. Getting total amount from array ==============
+// function getTotalAmount ($values, $currency)
+// {
+//     $sum = 0;
+//     foreach($values as $value)
+//     {   
+//         list($cur, $amount) = explode(' ', $value);
+//         if ($cur == $currency)
+//         {
+//             $sum += $amount;
+//         }
+//     }
+//     return $sum;
+// }
