3ba7f2d5d9ac0fc2a306db67060d5ac61c447d35
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-30 13:28:48 +0300

    added formatting html code from array

--- lessons.php
+++ lessons.php
@@ -223,3 +223,17 @@
 //     }
 //     return $sum;
 // }
+
+// =========== Task 19. Formatting html code from array ==============
+// function buildDefinitionList($definitions)
+// {
+//     $parts = [];
+//     if (empty($definitions)) {
+//         return '';
+//     }
+//     foreach ($definitions as $item) {
+//         $parts[] = "<dt>{$item[0]}</dt><dd>{$item[1]}</dd>";
+//     }
+//     $code = implode('', $parts);
+//     return "<dl>{$code}</dl>";
+// }
