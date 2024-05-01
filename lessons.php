f104088ba8eb88a47bbbb1ffd4b192cbd55d5bd3
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-01 12:25:56 +0300

    added counting of unique elements of two arrays

--- lessons.php
+++ lessons.php
@@ -246,6 +246,14 @@
 //     foreach ($words as $word) {
 //         $result[] = in_array($word, $stopWords) ? '$#%!' : $word;
 //     }
-
 //     return implode(' ', $result);
+// }
+
+// =========== Task 21. Counting of unique elements of two arrays ==============
+// function getSameCount(array $first, array $second)
+// {
+//     $unique1 = array_unique($first);
+//     $unique2 = array_unique($second);
+//     $intersection = array_intersect($unique1, $unique2);
+//     return count($intersection);
 // }
