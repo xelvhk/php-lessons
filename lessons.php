65c3cc603599d4fde28d5d7862a0a7b35b1e67e6
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-03 00:01:29 +0300

    added sorting array with bubble method

--- lessons.php
+++ lessons.php
@@ -257,3 +257,22 @@
 //     $intersection = array_intersect($unique1, $unique2);
 //     return count($intersection);
 // }
+
+// =========== Task 22. Sorting array with bubble method ==============
+// function bubbleSort($array)
+// {
+//     $num = count($array);
+//     do {
+//         $swapped = false;
+//         for ($i = 0; $i < $num - 1; $i++) {
+//             if ($array[$i] > $array[$i + 1]) {
+//                 $temp = $array[$i];
+//                 $array[$i] = $array[$i + 1];
+//                 $array[$i + 1] = $temp;
+//                 $swapped = true;
+//             }
+//         }
+//         $num--;
+//     } while ($swapped);
+//     return $array;
+// }
