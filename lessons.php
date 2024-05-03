429dce9037251c09824fe75e268d4d83ea636e6d
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-03 12:56:47 +0300

    added happy ticket task

--- lessons.php
+++ lessons.php
@@ -276,3 +276,17 @@
 //     } while ($swapped);
 //     return $array;
 // }
+
+// =========== Task 22. Happy ticket ==============
+// function isHappy (string $num)
+// {
+//     $trim = strlen($num)/2;
+//     $partOne = array_sum(str_split(substr($num, 0, $trim)));
+//     $partTwo = array_sum(str_split(substr($num, $trim)));
+//     if ($partOne == $partTwo) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+//     }
