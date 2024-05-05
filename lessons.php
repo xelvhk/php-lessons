631ef2b0ad1bf71fb8aa59f1baa501e7e5cbef11
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-05 18:17:09 +0300

    added searching for intersections of sorted arrays

--- lessons.php
+++ lessons.php
@@ -290,3 +290,22 @@
 //         return false;
 //     }
 //     }
+
+// =========== Task 23. Searching for intersections of sorted arrays ==============
+// function getIntersectionOfSortedArray($arr1, $arr2) {
+//     $result = [];
+//     $i = 0;
+//     $j = 0;
+//     while ($i < count($arr1) && $j < count($arr2)) {
+//         if ($arr1[$i] === $arr2[$j]) {
+//             $result[] = $arr1[$i];
+//             $i++;
+//             $j++;
+//         } elseif ($arr1[$i] > $arr2[$j]) {
+//             $j++;
+//         } else {
+//             $i++;
+//         }
+//     }
+//     return $result;
+// }
