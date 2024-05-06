7b1e4c6c066462fcf1e30b1e63fafa93ad23f92a
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-06 11:45:36 +0300

    added array flattening

--- lessons.php
+++ lessons.php
@@ -366,3 +366,16 @@
 //     }
 //     return $nearestLocation;
 // }
+
+// =========== Task 27. Array flattening ==============
+// function flatten($array) {
+//     $result = [];
+//     foreach ($array as $item) {
+//         if (is_array($item)) {
+//             $result = [...$result, ...$item];
+//         } else {
+//             $result[] = $item;
+//         }
+//     }
+//     return $result;
+// }
