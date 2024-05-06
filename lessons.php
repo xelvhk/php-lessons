d88d657aa417ecb700cd8a46056e02d8e9fb0de3
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-06 11:29:04 +0300

    added searching for nearest locations v.2

--- lessons.php
+++ lessons.php
@@ -310,7 +310,7 @@
 //     return $result;
 // }
 
-// =========== Task 25. Searching for nearest locations ==============
+// =========== Task 25. Searching for nearest locations v.1 ==============
 // function getDistance(array $point1, array $point2)
 // {
 //     [$x1, $y1] = $point1;
@@ -333,6 +333,34 @@
 //         $distance = getDistance($currentPoint, $point);
 //         if ($distance < $lowestDistance) {
 //             $lowestDistance = $distance;
+//             $nearestLocation = $location;
+//         }
+//     }
+//     return $nearestLocation;
+// }
+
+
+// =========== Task 26. Searching for nearest locations v.2 ==============
+// function getDistance(array $point1, array $point2)
+// {
+//     [$x1, $y1] = $point1;
+//     [$x2, $y2] = $point2;
+//     $xs = $x2 - $x1;
+//     $ys = $y2 - $y1;
+//     return sqrt($xs ** 2 + $ys ** 2);
+// }
+
+// function getTheNearestLocation($locations, $point) {
+//     if (empty($locations)) {
+//         return null;
+//     }
+//     $nearestLocation = null;
+//     $minDistance = PHP_INT_MAX;
+//     foreach ($locations as $location) {
+//         $locationPoint = $location[1];
+//         $distance = getDistance($point, $locationPoint);    
+//         if ($distance < $minDistance) {
+//             $minDistance = $distance;
 //             $nearestLocation = $location;
 //         }
 //     }
