164994c53112be64450c9ba0aa95caea9b0aab0c
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-29 09:32:16 +0300

    Create 4 - Simple calculator.php

---
+++ 4 - Simple calculator.php
@@ -0,0 +1,15 @@
+// =========== Task 4. Simple calculator ==============
+// function calculate($op,$num1,$num2) {
+//     switch ($op){
+//         case '+':
+//             return $num1 + $num2;
+//         case '-':
+//             return $num1 - $num2;
+//         case '/':
+//             return $num1 / $num2;
+//         case '*':
+//             return $num1 * $num2;
+//         default:
+//             return null;
+//     }
+// }
