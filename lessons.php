1b48bd4d5f0344e156d09800597a852e30d63354
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-31 15:04:17 +0300

    Update lessons.php

--- lessons.php
+++ lessons.php
@@ -1,26 +1,3 @@
-// =========== Task 6. Сase inversion ==============
-// function invertCase($text) {
-//     $result = '';
-//     for ($i = 0; $i < mb_strlen($text); $i += 1)
-//     {
-//         $temp = mb_substr($text,$i,1);
-//         if ($temp === mb_strtoupper($temp)){
-//             $result = $result.mb_strtolower($temp);
-//         }
-//         else {
-//             $result = $result.mb_strtoupper($temp);
-//         }   
-//     } 
-//     return $result;
-// }
-
-// =========== Task 7. Timestamp format ==============
-// function getCustomDate($time)
-// {
-//  $fdate = date('d/m/Y', $time);
-//  return $fdate;
-// }
-
 // =========== Task 8. Palindrom check function ==============
 // function isPalindrome(string $word) {
 //     $lastIndex = mb_strlen($word) - 1;
