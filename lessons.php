5db0c8dec5b54ca2e291dc4dc18c37001612fe01
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-01 15:54:34 +0300

    Update lessons.php

--- lessons.php
+++ lessons.php
@@ -1,17 +1,3 @@
-// =========== Task 8. Palindrom check function ==============
-// function isPalindrome(string $word) {
-//     $lastIndex = mb_strlen($word) - 1;
-//     $middleIndex = $lastIndex / 2;
-//     for ($i = 0; $i < $middleIndex; $i++) {
-//         $symbol = mb_substr($word, $i, 1);
-//         $mirroredSymbol = mb_substr($word, $lastIndex - $i, 1);
-//         if ($symbol !== $mirroredSymbol) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
 // =========== Task 9. Vowel check function ==============
 // namespace App\Symbols;
 // function isVowel($char)
