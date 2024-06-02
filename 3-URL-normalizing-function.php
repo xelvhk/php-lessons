c7633ab9dd9eb7a382c1799b36ee976575d5251f
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-03 00:05:23 +0300

    Update and rename 3 - URL normalizing function.php to 3-URL-normalizing-function.php

---
+++ 3-URL-normalizing-function.php
@@ -0,0 +1,13 @@
+<?php
+
+=========== Task 3. URL normalizing function ==============
+function normalizeUrl($url) {
+    if (strpos($url, 'http://') === 0) {
+        $domain = substr($url, 7);
+    } elseif (strpos($url, 'https://') === 0) {
+        $domain = substr($url, 8);
+    } else {
+        $domain = $url;
+    }
+    return "https://{$domain}";
+}
