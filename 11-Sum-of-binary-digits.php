ce98c4786d9321c3187a6b9fe6e310087b5ec6a0
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-04 22:35:11 +0300

    Create 11-Sum-of-binary-digits.php

---
+++ 11-Sum-of-binary-digits.php
@@ -0,0 +1,7 @@
+<?php
+// =========== Task 11. Sum of binary digits ==============
+function binarySum (string $num1, string $num2)
+{
+    $sum = bindec($num1) + bindec($num2);
+    return decbin($sum);
+}
