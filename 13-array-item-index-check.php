503ce9b159c1059f66749cad470d7e4423a936c7
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-08 00:22:44 +0300

    Create 13-array-item-index-check.php

---
+++ 13-array-item-index-check.php
@@ -0,0 +1,5 @@
+// =========== Task 13. Array item index check with ternary operator ==============
+function get(array $arr, int $index, $default = null)
+{
+  return array_key_exists($index, $arr) ? $arr[$index] : $default;
+}
