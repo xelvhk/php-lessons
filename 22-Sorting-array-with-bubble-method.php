14b6f677e0f6e8d5e32a5327ac196fe20a479ca2
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-10 13:20:32 +0300

    Create 22-Sorting-array-with-bubble-method.php

---
+++ 22-Sorting-array-with-bubble-method.php
@@ -0,0 +1,20 @@
+<?php
+
+// =========== Task 22. Sorting array with bubble method ==============
+function bubbleSort($array)
+{
+    $num = count($array);
+    do {
+        $swapped = false;
+        for ($i = 0; $i < $num - 1; $i++) {
+            if ($array[$i] > $array[$i + 1]) {
+                $temp = $array[$i];
+                $array[$i] = $array[$i + 1];
+                $array[$i + 1] = $temp;
+                $swapped = true;
+            }
+        }
+        $num--;
+    } while ($swapped);
+    return $array;
+}
