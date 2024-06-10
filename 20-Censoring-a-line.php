c56d33ce67676b33c4c215a1dc1ec6c66cd721aa
Lex <166012830+xelvhk@users.noreply.github.com>
2024-06-10 13:18:13 +0300

    Create 20-Censoring-a-line.php

---
+++ 20-Censoring-a-line.php
@@ -0,0 +1,12 @@
+<?php
+
+// =========== Task 20. Censoring a line ==============
+function makeCensored(string $text, array $stopWords)
+{
+    $words = explode(' ', $text);
+    $result = [];
+    foreach ($words as $word) {
+        $result[] = in_array($word, $stopWords) ? '$#%!' : $word;
+    }
+    return implode(' ', $result);
+}
