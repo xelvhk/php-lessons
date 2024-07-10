31f0a2567c953226ef9067fde26c423a349d1081
Lex <166012830+xelvhk@users.noreply.github.com>
2024-07-10 21:42:33 +0300

    Update 34-Get-middle-point.php

--- 34-Get-middle-point.php
+++ 34-Get-middle-point.php
@@ -1,14 +1,36 @@
 <?php
 
-function getMidpoint($point1,$point2) {
+// v.1
+function getMidpoint($point1,$point2)
+{
     $result = ['x' => ($point1['x'] + $point2['x']) / 2,'y' => ($point1['y'] + $point2['y']) / 2];
     return $result;
 }
 
+// v.2
 // function getMidpoint($point1, $point2)
 // {
 //     $x = ($point1['x'] + $point2['x']) / 2;
 //     $y = ($point1['y'] + $point2['y']) / 2;
 
 //     return [ 'x' => $x, 'y' => $y ];
+// }
+
+// v.3 hiding functions
+
+// $point1 = ['x' => 0, 'y' => 0];
+// $point2 = ['x' => 4, 'y' => 3];
+
+// function getX($point1, $point2){
+//     $x = ($point1['x'] + $point2['x']) / 2;
+//     return $x;
+// };
+// function getY($point1, $point2)
+// {
+//     $y = ($point1['y'] + $point2['y']) / 2;
+//   return $y;
+// };
+// function getMidpoint($point1, $point2)
+// {
+// return [ 'x' => getX($point1, $point2), 'y' => getY($point1, $point2) ];
 // }
