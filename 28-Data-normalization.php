7fc42839667cf338a5a9be404305c3bf42fa4d15
Lex <166012830+xelvhk@users.noreply.github.com>
2024-08-31 23:32:51 +0300

    Update 28-Data-normalization.php

--- 28-Data-normalization.php
+++ 28-Data-normalization.php
@@ -1,5 +1,5 @@
 <?php
-
+// =========== Task 27. Data normalization ==============
 function normalize(&$lesson) {
     $lesson['name'] = mb_convert_case($lesson['name'], MB_CASE_TITLE, "UTF-8");
     $lesson['description'] = mb_strtolower($lesson['description']);
